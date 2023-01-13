e6a84aef47319b045cc62422844d252ca9418bde
McCoy Didericksen <mccoy.did@gmail.com>
2023-01-13 12:31:18 -0700

    switch to category api

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,15 +1,23 @@
 
 
 function getTriviaData() {
-    const categoryUrl = 'https://jservice.io/api/random'
     let triviaData = [];
     for(let i=0; i<4; i++){
+        const categoryCnt = 6000; 
+        let randomInt = Math.floor(Math.random() * (categoryCnt - 1) + 1);
+        let categoryUrl = 'https://jservice.io/api/category?id=' + randomInt;
         fetch(categoryUrl)
         .then(function(response) {
             response.json()
         .then(function(data){
             console.log(data);
-
+            let triviaObj = {
+                category: data.title
+            }
+            // let categoryQuestions = category.clues;
+            // categoryQuestions.sort(function (a, b) {
+            //     return b.score - a.score
+            })
             })
         })
     }
