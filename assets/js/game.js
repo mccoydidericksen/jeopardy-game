a4b574ca167b5a184572f51f269260c06598d6e1
McCoy Didericksen <mccoy.did@gmail.com>
2023-01-13 13:12:21 -0700

    add questions object to output array and implement comments

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,27 +1,30 @@
 
 
-function getTriviaData() {
+function getRandomTriviaData() {
     let triviaData = [];
+    const categoryCnt = 28163;
+    // fetch 4 random trivia categories
     for(let i=0; i<4; i++){
-        const categoryCnt = 6000; 
+        //gets random category id (categoryCnt is the total amount of categories available)
         let randomInt = Math.floor(Math.random() * (categoryCnt - 1) + 1);
         let categoryUrl = 'https://jservice.io/api/category?id=' + randomInt;
         fetch(categoryUrl)
         .then(function(response) {
             response.json()
         .then(function(data){
-            console.log(data);
             let triviaObj = {
-                category: data.title
+                category: data.title,
+                questions: {}
             }
-            // let categoryQuestions = category.clues;
-            // categoryQuestions.sort(function (a, b) {
-            //     return b.score - a.score
-            })
+            let categoryQuestions = data.clues;
+            // building question object with keys of the difficulty value
+            for(let i=0; i<categoryQuestions.length; i++){
+                triviaObj.questions[categoryQuestions[i].value] = categoryQuestions[i];
+            }
+            triviaData.push(triviaObj);
             })
         })
     }
-
+    return triviaData;
 }
 
-getTriviaData();
