bd4613eeb8aa997cd9202bb815dd93ac215d4316
McCoy Didericksen <mccoy.did@gmail.com>
2023-01-15 11:35:54 -0700

    create html elements for testing

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,11 @@
-buttonEl = $("#startButton");
+const buttonEl = $("#startButton");
+const highScoreEl = $("#highScore");
+// const globalScores = getHighScores();
 
 // add event listener to call the game.html
 buttonEl.on("click", function () {
   window.location = "./game.html";
 });
+
+
+
